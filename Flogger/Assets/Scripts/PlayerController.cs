2f7c48fadd09b7062f6a34e9aab9478d51c80547
gilzoide <gilzoide@gmail.com>
2016-11-27 16:26:41 -0200

    Projeto na Unity criado, sapim placeholder anda zuado ainda

---
+++ Flogger/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	const int ladoQuadrado = 1;
+
+	// Update is called once per frame
+	void Update () {
+		var h = 0f;
+		var v = 0f;
+		if (Input.GetKeyDown (KeyCode.UpArrow)) v += 1;
+		if (Input.GetKeyDown (KeyCode.DownArrow)) v -= 1;
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) h -= 1;
+		if (Input.GetKeyDown (KeyCode.RightArrow)) h += 1;
+
+
+		transform.Translate (h * ladoQuadrado, v * ladoQuadrado, 0);
+	}
+}
