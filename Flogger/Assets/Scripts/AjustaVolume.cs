60386d6bdb6ad39e257a4e7b03887360f91b52a0
gilzoide <gilzoide@gmail.com>
2016-12-03 16:31:02 -0200

    Audio mixer, baixano música quando video fala =]

---
+++ Flogger/Assets/Scripts/AjustaVolume.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AjustaVolume : MonoBehaviour {
+
+	public AudioMixer mixer;
+
+	public void SetSfxVol (float novo) {
+		mixer.SetFloat ("sfxVol", novo);
+	}
+
+	public void SetMusicaVol (float novo) {
+		mixer.SetFloat ("musicaVol", novo);
+	}
+
+	public void SetVideoVol (float novo) {
+		mixer.SetFloat ("videoVol", novo);
+	}
+}
